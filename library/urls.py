4a9fedbd0d1ad7813ea51fbcc5078d07698f763f
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-10-11 22:36:57 +0300

    add urls.py library

---
+++ library/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from library.apps import LibraryConfig
+from library.views import AuthorCreateAPIView, AuthorListAPIView, AuthorRetrieveAPIView, AuthorUpdateAPIView, \
+    AuthorDestroyAPIView, BookCreateAPIView, BookListAPIView, BookRetrieveAPIView, BookUpdateAPIView, BookDestroyAPIView
+app_name = LibraryConfig.name
+urlpatterns = [
+    path('author-create/', AuthorCreateAPIView.as_view(), name='author-create'),
+    path('author-retrieve/<int:pk>/', AuthorRetrieveAPIView.as_view(), name='author-retrieve'),
+    path('author-update/<int:pk>/', AuthorUpdateAPIView.as_view(), name='author-update'),
+    path('author-destroy/<int:pk>/', AuthorDestroyAPIView.as_view(), name='author-destroy'),
+    path('author-list/', AuthorListAPIView.as_view(), name='author-list'),
+    path('book-create/', BookCreateAPIView.as_view(), name='book-create'),
+    path('book-retrieve/<int:pk>/', BookRetrieveAPIView.as_view(), name='book-retrieve'),
+    path('book-update/<int:pk>/', BookUpdateAPIView.as_view(), name='book-update'),
+    path('book-destroy/<int:pk>/', BookDestroyAPIView.as_view(), name='book-destroy'),
+    path('book-list/', BookListAPIView.as_view(), name='book-list')
+]
