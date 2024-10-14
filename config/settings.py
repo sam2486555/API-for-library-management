1cc55f85a6dbddcdcb790a22745a00e743ce70e7
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-10-15 00:15:52 +0300

    changes

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
