fb9c2954f7738994e6e78fd7c8ff50639d2096d7
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-10-15 00:07:42 +0300

    changes

--- library/urls.py
+++ library/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from library.apps import LibraryConfig
 from library.views import AuthorCreateAPIView, AuthorListAPIView, AuthorRetrieveAPIView, AuthorUpdateAPIView, \
     AuthorDestroyAPIView, BookCreateAPIView, BookListAPIView, BookRetrieveAPIView, BookUpdateAPIView, BookDestroyAPIView
+
 app_name = LibraryConfig.name
 urlpatterns = [
     path('author-create/', AuthorCreateAPIView.as_view(), name='author-create'),
@@ -13,5 +14,6 @@ urlpatterns = [
     path('book-retrieve/<int:pk>/', BookRetrieveAPIView.as_view(), name='book-retrieve'),
     path('book-update/<int:pk>/', BookUpdateAPIView.as_view(), name='book-update'),
     path('book-destroy/<int:pk>/', BookDestroyAPIView.as_view(), name='book-destroy'),
-    path('book-list/', BookListAPIView.as_view(), name='book-list')
+    path('book-list/', BookListAPIView.as_view(), name='book-list'),
+    # path('book-status/<int:pk>/', BookStatusAPIView, name='book-status')
 ]
